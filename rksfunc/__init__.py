71cbb5cb6b60be2d8245500ed571632a05500dde
RyougiKukoc <st_stars@163.com>
2023-03-31 21:09:01 +0800

    Revert "for wildcard imports"
    This reverts commit 0e4e012e8f8ae015bf880beb0c3feab5ae42a445.

--- rksfunc/__init__.py
+++ rksfunc/__init__.py
@@ -5,9 +5,3 @@ from ._mask import *
 from ._denoise import *
 from ._source import *
 from ._collect_wraper import *
-
-_mods = ['_resample', '_mask', '_denoise', '_source', '_collect_wraper']
-
-__all__ = []
-for _pkg in _mods:
-    __all__ += __import__(__name__ + '.' + _pkg, fromlist=_mods).__all__
