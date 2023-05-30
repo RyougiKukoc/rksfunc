b66091ab2872d1339b00d4009bda1917eb5fce6b
RyougiKukoc <st_stars@163.com>
2023-05-30 11:49:52 +0800

    Update _mask.py

--- rksfunc/_mask.py
+++ rksfunc/_mask.py
@@ -3,23 +3,21 @@ from functools import partial
 from ._resample import *
 
 
-def scanny(c16: VideoNode) -> VideoNode:
+def scanny(clip: VideoNode) -> VideoNode:
     """
-    Create a simple TCanny deband mask.
-    :param c16: input YUV420P16 clip.
+    Create a simple TCanny deband mask. Stolen from LoliHouse.
+    :param clip: input clip.
     :return: result of mask.
     """
-    from vapoursynth import YUV420P16
+    from vapoursynth import GRAY16
     
-    c8 = c16.fmtc.bitdepth(bits=8)
+    c8 = yer(c16.fmtc.bitdepth(bits=8))
     TC = core.tcanny.TCanny
-    masks = TC(c8, 0.8, op=2, mode=1, planes=[0, 1, 2]) \
-        .std.Expr(["x 7 < 0 65535 ?", "x 256 *"], YUV420P16)
+    masks = TC(c8, 0.8, op=2, mode=1).std.Expr("x 7 < 0 65535 ?", GRAY16)
     maskb = TC(c8, 1.3, t_h=6.5, op=2, planes=0)
     maskg = TC(c8, 1.1, t_h=5.0, op=2, planes=0)
-    mask = core.std.Expr([maskg, maskb, masks, c8], \
-        ["a 20 < 65535 a 48 < x 256 * a 96 < y 256 * z ? ? ?", "z"], YUV420P16)
-    return mask.std.Maximum(0).std.Maximum(0).std.Minimum(0).rgvs.RemoveGrain([20, 0])
+    mask = core.std.Expr([maskg, maskb, masks, c8], "a 20 < 65535 a 48 < x 256 * a 96 < y 256 * z ? ? ?", GRAY16)
+    return mask.std.Maximum(0).std.Maximum(0).std.Minimum(0).rgvs.RemoveGrain(20)
 
 
 insane_deband_mask = scanny
